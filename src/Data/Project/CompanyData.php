0643d395e8ad2b99f04bdcf9db8f50544a91fccd
Vaclav Gryc <gryc.vaclav@gmail.com>
2025-06-01 22:36:35 +0200

    Improved CompanyData

--- src/Data/Project/CompanyData.php
+++ src/Data/Project/CompanyData.php
@@ -9,5 +9,16 @@ class CompanyData extends Data
     public function __construct(
         public int $id,
         public string $name,
+        public ?string $addressOne,
+        public ?string $addressTwo,
+        public ?string $city,
+        public ?string $companyNameUrl,
+        public ?string $countryCode,
+        public ?string $createdAt,
+        public ?string $emailOne,
+        public ?string $emailTwo,
+        public ?string $emailThree,
+        public ?string $state,
+        public ?string $updatedAt,
     ) {}
 }
